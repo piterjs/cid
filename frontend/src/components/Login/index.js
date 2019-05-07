878d8cfcd606235d48480699164692a2a49eb455
Andrey Gurylev <andrey.gurylev@team.wrike.com>
2019-05-07 21:18:16 +0300

    closed CID-1; added styles for login page

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -5,17 +5,21 @@ import history from '../../history';
 import './styles.css';
 
 export default () => (
-  <div className="loginform">
-    <h1>Welcome!</h1>
-    <label>
-      Name: <input type="text" />
-    </label>
-    <label>
-      Email: <input type="email" />
-    </label>
-    <label>
-      Secret: <input type="password" />
-    </label>
-    <button onClick={() => history.push('/editor')}>Play</button>
-  </div>
+  <form className="form form--login" onSubmit={() => history.push('/editor')}>
+    <h1>welcome!</h1>
+    <h3>you can wear a blindfold</h3>
+    <div className="form__line">
+      <label htmlFor="login" className="form__label">login:</label>
+      <input type="text" id="login" placeholder="@fogrew" autoFocus className="form__control" required/>
+      <div className="form__hint">for example, telegram login</div>
+    </div>
+    <div className="form__line">
+      <label htmlFor="email" className="form__label">email:</label>
+      <input type="email" id="email" placeholder="hello@piterjs.org" className="form__control" required/>
+      <div className="form__hint">we will write you if you win</div>
+    </div>
+    <div className="form__line">
+      <button className="button button--shine">PLAY</button>
+    </div>
+  </form>
 );
